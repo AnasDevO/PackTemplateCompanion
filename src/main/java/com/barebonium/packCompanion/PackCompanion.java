89e3a7176ae5f106fc447b92a8740714936c8f12
Thermonuclear_Minecraft_Engineer <best1chanel@gmail.com>
2026-01-22 15:47:29 +0100

    Initial Commit

--- src/main/java/com/barebonium/packCompanion/PackCompanion.java
+++ src/main/java/com/barebonium/packCompanion/PackCompanion.java
@@ -1,15 +1,17 @@
-package com.example.modid;
+package com.barebonium.packCompanion;
 
-import com.example.modid.Tags;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class PackCompanion {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+
+    public void onPreInit(FMLPreInitializationEvent event) {}
 
     /**
      * <a href="https://cleanroommc.com/wiki/forge-mod-development/event#overview">
@@ -18,7 +20,11 @@ public class ExampleMod {
      */
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+
+        LOGGER.info("{} is Checking your modlist!", Tags.MOD_NAME);
+
+
     }
+
 
 }
---
+++ src/main/java/com/barebonium/packCompanion/utils/ModEntry.java
@@ -0,0 +1,8 @@
+package com.barebonium.packCompanion.utils;
+
+public class ModEntry {
+    public String modId;
+    public String version;
+    public String replacementModName;
+    public String message;
+}
